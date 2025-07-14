ea825097be6671e165fe1cf07f6d061adb87db36
pasqualedimaro <pakydm15@gmail.com>
2025-07-14 19:01:24 +0200

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
             <div className="container-icone">
                <nav>
                     <ul>
-                        <li><img src="/img/buy-comics-digital-comics.png" alt="" /><span>DIGITAL COMICS</span></li>
-                        <li><img src="/img/buy-comics-merchandise.png" alt="" /><span>DC MERCHANDISE</span></li>
-                        <li><img src="/img/buy-comics-subscriptions.png" alt="" /><span>SUBSCRIPTION</span></li>
-                        <li><img src="/img/buy-comics-shop-locator.png" alt="" /><span>COMIC SHOP LOCATOR</span></li>
-                        <li><img src="/img/buy-dc-power-visa.svg" alt="" /><span>DC POWER VISA</span></li>
+                       <li><a href="#"><img src="/img/buy-comics-digital-comics.png" alt="" /></a><span>DIGITAL COMICS</span></li>
+                        <li><a href="#"><img src="/img/buy-comics-merchandise.png" alt="" /></a><span>DC MERCHANDISE</span></li>
+                        <li><a href="#"><img src="/img/buy-comics-subscriptions.png" alt="" /></a><span>SUBSCRIPTION</span></li>
+                        <li><a href="#"><img src="/img/buy-comics-shop-locator.png" alt="" /></a><span>COMIC SHOP LOCATOR</span></li>
+                        <li><a href="#"><img src="/img/buy-dc-power-visa.svg" alt="" /></a><span>DC POWER VISA</span></li>
                     </ul>
                 </nav>   
             </div>
@@ -19,57 +19,58 @@ function Footer() {
                 <nav>
                     <ul>
                         <h3>DC COMICS</h3>
-                        <li>Characters</li>
-                        <li>Comic</li>
-                        <li>Movies</li>
-                        <li>TV</li>
-                        <li>Games</li>
-                        <li>Videos</li>
-                        <li>News</li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comic</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
                         <h3>SHOP</h3>
-                        <li>Shop DC</li>
-                        <li>Shop DC Collectibles</li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
                     </ul>
                 </nav>
                 <nav>
                     <ul>
                         <h3>DC</h3>
-                        <li>Terms Of Use</li>
-                        <li>Privacy policy(New)</li>
-                        <li>Ad Choises</li>
-                        <li>Adverstising</li>
-                        <li>Jobs</li>
-                        <li>Subscriptions</li>
-                        <li>Talent Workshops</li>
-                        <li>CPSC Certificates</li>
-                        <li>Ratings</li>
-                        <li>Shop Help</li>
-                        <li>Contact Us</li>
+                         <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy(New)</a></li>
+                        <li><a href="#">Ad Choises</a></li>
+                        <li><a href="#">Adverstising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
                     </ul>
                 </nav>
                 <nav>
                     <ul>
                         <h3>SITES</h3>
                         <li>DC</li>
-                        <li>MAD Magazine</li>
-                        <li>DC Kids</li>
-                        <li>DC Universe</li>
-                        <li>DC Power Visa</li>
+                         <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
                     </ul>
                 </nav>
             </div>
             <div className='container-social'>
                 <nav>
-                    <button className='btn'>SIGN-UP NOW!</button>
+                    <button className='btn'><a href="#">SIGN-UP NOW!</a></button>
                 </nav>
                 <nav>
                     <ul>
                         <h2>FOLLOW US</h2>
-                        <il><img src="/img/footer-facebook.png" alt="" /></il>
-                        <il><img src="/img/footer-twitter.png" alt="" /></il>
-                        <il><img src="/img/footer-youtube.png" alt="" /></il>
-                        <il><img src="/img/footer-pinterest.png" alt="" /></il>
-                        <il><img src="/img/footer-periscope.png" alt="" /></il>
+                        <il><a href="#"><img src="/img/footer-facebook.png" alt="" /></a></il>
+                        <il><a href="#"><img src="/img/footer-twitter.png" alt="" /></a></il>
+                        <il><a href="#"><img src="/img/footer-youtube.png" alt="" /></a></il>
+                        <il><a href="#"><img src="/img/footer-pinterest.png" alt="" /></a></il>
+                        <il><a href="#"><img src="/img/footer-periscope.png" alt="" /></a></il>
                     </ul>
                 </nav>
             </div>
