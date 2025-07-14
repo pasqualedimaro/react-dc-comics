d1108da6970f0e13caf4479f8045ffef2910592c
pasqualedimaro <pakydm15@gmail.com>
2025-07-14 17:11:29 +0200

    add footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,17 @@
 function Footer() {
     return (
-        <div>Footer</div>
+       <footer>
+            <div className="container-icone">
+                <section>dc</section>
+                <section>maglia</section>
+                <section>sub</section>
+                <section>comic</section>
+                <section>power</section>
+            </div>
+            <div>liste con sfondo</div>
+            <div>barra social</div>
+        </footer>
+
     )
 }
 
